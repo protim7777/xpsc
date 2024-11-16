8fee33267e2ac99cd476c24ac290d36ea65d3ce2
partho protim sen <157377182+protim7777@users.noreply.github.com>
2024-11-17 00:17:46 +0600

    G-CompareT-Shirt_Sizes.cpp

--- week4 Day 1/codeforce_1741A.cpp
+++ week4 Day 1/codeforce_1741A.cpp
@@ -7,24 +7,23 @@ int main()
 
     while(t--)
     {
-    string s1,s2;
-    cin >>s1>>s2;
-
-    if(s1[s1.size()-1]=='M' || s2[s2.size()-1]=='M' )
+    string s,s2;
+    cin >>s>>s2;
+    if(s[s.size()-1]=='M' || s2[s2.size()-1]=='M' )
     {
-        if(s1[s1.size()-1] == 'M' && s2[s2.size()-1]=='S')
+        if(s[s.size()-1] == 'M' && s2[s2.size()-1]=='S')
         {
             cout<<'>'<<endl;
         }
-        else if(s1[s1.size()-1] == 'S' && s2[s2.size()-1]=='M')
+        else if(s[s.size()-1] == 'S' && s2[s2.size()-1]=='M')
         {
             cout<<'<'<<endl;
         }
-        else if(s1[s1.size()-1] == 'M' && s2[s2.size()-1]=='L')
+        else if(s[s.size()-1] == 'M' && s2[s2.size()-1]=='L')
         {
             cout<<'<'<<endl;
         }
-        else if(s1[s1.size()-1] == 'L' && s2[s2.size()-1]=='M')
+        else if(s[s.size()-1] == 'L' && s2[s2.size()-1]=='M')
         {
             cout<<'>'<<endl;
         }
@@ -35,25 +34,43 @@ int main()
     }
     else
     {
-        if(s1[s1.size()-1] == 'S' && s2[s2.size()-1]=='L')
+        if(s[s.size()-1] == 'S' && s2[s2.size()-1]=='L')
         {
             cout<<'<'<<endl;
         }
-        else if(s1[s1.size()-1] == 'L' && s2[s2.size()-1]=='S')
+        else if(s[s.size()-1] == 'L' && s2[s2.size()-1]=='S')
         {
             cout<<'>'<<endl;
         }
-        else if(s1[s1.size()-1] == 'L' && s2[s2.size()-1]=='L')
+        else if(s[s.size()-1] == 'L' && s2[s2.size()-1]=='L')
         {
-            if(s1.size()>s2.size()) cout<<'>'<<endl;
-            else if(s1.size()< s2.size()) cout<<'<'<<endl;
-            else cout<<'='<<endl;
+            if(s.size()>s2.size()) 
+            {
+                cout<<'>'<<endl;
+            }    
+            else if(s.size()< s2.size()) 
+            {
+                cout<<'<'<<endl;
+            }    
+            else 
+            {
+                cout<<'='<<endl;
+            }    
         }
-        else if(s1[s1.size()-1] == 'S' && s2[s2.size()-1]=='S')
+        else if(s[s.size()-1] == 'S' && s2[s2.size()-1]=='S')
         {
-            if(s1.size()< s2.size()) cout<<'>'<<endl;
-            else if(s1.size()> s2.size()) cout<<'<'<<endl;
-            else cout<<'='<<endl;
+            if(s.size()< s2.size()) 
+            {
+                cout<<'>'<<endl;
+            }    
+            else if(s.size()> s2.size()) 
+            {
+                cout<<'<'<<endl;
+            }
+            else 
+            {
+                cout<<'='<<endl;
+            }    
         }
     }
     }
