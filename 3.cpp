9b47c77723559556837240f9e64431f66ab5475a
partho protim sen <157377182+protim7777@users.noreply.github.com>
2024-12-13 23:37:46 +0600

    Add files via upload

--- 3.cpp
+++ 3.cpp
@@ -1,18 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+int main() 
 {
     int t;
-    cin >>t;
-    for (int i=0;i<t;i++){
-        int a,b,c;
-        cin >>a>>b>>c;
-        if(a+b==c){
-            cout <<"+"<<endl;
+    cin >> t;
+    do{
+        int n;
+        cin>>n;
+        string s, t_input;
+        cin >> s >> t_input;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != t_input[i]) {
+                cnt++;
+            }
         }
-        else{
-            cout <<"-"<<endl;
-        }
-    }
+        cout << (cnt % 2 == 0 ? "YES" : "NO") << endl;
+        t--;
+    } while(t > 0);
     return 0;
 }
